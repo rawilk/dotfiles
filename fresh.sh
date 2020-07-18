dc456d549aa9254e8d73942a8a2774b744561f5f
Randall Wilk <wilk.randall@gmail.com>
2020-07-17 23:58:08 -0500

    tweak installation

--- fresh.sh
+++ fresh.sh
@@ -34,15 +34,6 @@ git checkout d6a36b1 agnoster.zsh-theme
 cd ~/.dotfiles/shell
 chmod +x z.sh
 
-# Composer
-echo 'Install composer'
-echo '----------------'
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
-
 # Homebrew
 echo 'Install homebrew'
 echo '----------------'
@@ -91,6 +82,9 @@ valet install
 
 # Create dev directories and clone git repos
 ./clone.sh
+
+# Install Powerline Patched Fonts
+./install-fonts.sh
 
 echo '++++++++++++++++++++++++++++++'
 echo '++++++++++++++++++++++++++++++'
---
+++ install-fonts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'Installing Patched Fonts...'
+echo '------------------------'
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+
+# clean-up a bit
+cd ..
+rm -rf fonts
