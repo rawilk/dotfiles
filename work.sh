63c651a772e906c6543cdb8957573fec5e273d1b
Randall Wilk <wilk.randall@gmail.com>
2020-08-17 07:31:00 -0500

    Add script to start apps for work

---
+++ work.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# launch websites
+open -a "Google Chrome.app" https://rite-dev.riverwoodeducational.com
+
+# other apps
+open -a "Spotify.app"
+open -a "Spark.app" # email
+
+# launch work projects
+open -a "PhpStorm.app" ~/dev/code/work/rite
