b2916e922143af251e9c9aca4b25ec2746112c2e
rawilk <wilk.randall@gmail.com>
2020-09-18 22:16:20 -0500

    Update repos

--- clone.sh
+++ clone.sh
@@ -25,9 +25,9 @@ VENDOR=$CODE/vendor
 git clone git@github.com:rawilk/randallwilk.dev.git $PERSONAL/randallwilk.dev
 
 # Work
-git clone git@github.com:cybrix-solutions/cybrixsolutions.com.git $WORK/cybrixsolutions.com
-git clone git@github.com:cybrix-solutions/rite.git $WORK/rite
-git clone git@github.com/rawilk/rite-v3.git $WORK/rite-v3
+git clone git@github.com/cybrix-solutions/cybrixsolutions.com.git $WORK/cybrixsolutions.com
+git clone git@github.com/cybrix-solutions/rite.git $WORK/rite
+git clone git@github.com/cybrix-solutions/rite-v3.git $WORK/rite-v3
 
 # Packages
 git clone git@github.com:rawilk/laravel-printing.git $PACKAGES/laravel-printing
@@ -35,11 +35,12 @@ git clone git@github.com:rawilk/shipper.git $PACKAGES/shipper
 git clone git@github.com:rawilk/laravel-app-key-rotator.git $PACKAGES/laravel-app-key-rotator
 git clone git@github.com/rawilk/laravel-settings.git $PACKAGES/laravel-settings
 git clone git@github.com/rawilk/laravel-breadcrumbs.git $PACKAGES/laravel-breadcrumbs
+git clone git@github.com/rawilk/laravel-casters.git $PACKAGES/laravel-casters
+git clone git@github.com/rawilk/laravel-form-components.git $PACAKGES/laravel-form-components
 git clone git@github.com/rawilk/package-skeleton-laravel.git $PACKAGES/package-skeleton-laravel
 
 # NPM Packages
 git clone git@github.com:rawilk/vue-context.git $NPM/vue-context
-git clone git@github.com:rawilk/vue-context-docs.git $NPM/vue-context-docs
 
 # Vendor Repos
 git clone git@github.com:envault/envault.git $VENDOR/envault
