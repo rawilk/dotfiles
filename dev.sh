5a68685086fb6cf57bbbf0c16e171fdace7dcffa
Randall Wilk <wilk.randall@gmail.com>
2022-05-09 14:31:20 -0500

    Add puppeteer installation

--- dev.sh
+++ dev.sh
@@ -37,6 +37,10 @@ echo '-------------'
 mkdir "${HOME}/.npm-packages"
 npm config set prefix "${HOME}/.npm-packages"
 
+# Puppeteer
+# - Necessary for spatie browser shot package
+npm install puppeteer --global
+
 # Create dev directories and clone git repos
 $DOTFILES/clone.sh
 
